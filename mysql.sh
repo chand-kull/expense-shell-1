45fe4cce5c9c7c3dece281104f8a47a73bd2e151
chandu <cheerlachandrasekhar81@gmail.com>
2025-11-12 19:54:11 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -3,8 +3,8 @@ source ./common.sh
 
 check_root()
 
-echo "please enter DB password:"
-read -s mysql_root_password
+ echo "please enter DB password:"
+ read  -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "installing mysql-server"
